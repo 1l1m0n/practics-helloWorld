6dddaabe6e9baea24a4b008ec871156599a6a0da
1l1m0n <sv.antonov1@gmail.com>
2025-07-07 22:38:46 +0200

    DB + test users + properties added

--- hello/src/main/java/com/antonovproject/hello/HelloController.java
+++ hello/src/main/java/com/antonovproject/hello/HelloController.java
@@ -1,13 +1,38 @@
 package com.antonovproject.hello;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class HelloController {
     
     @RequestMapping("/helloWorld")
     public String helloWorld() {
-        return "Hello Antonov!!!";
+        return "Hello Antonov!!!!!";
+    }
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestParam String name) {
+
+        User user = new User();
+        user.setName(name);
+        userRepo.save(user);
+        return "User added and save: " + name;
+    }
+    
+    @GetMapping("/getUsers")
+    public List<User> getUsers() {
+        return userRepo.findAll();
     }
 }
