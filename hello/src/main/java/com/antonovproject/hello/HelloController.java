eb78d848d9b14e3b314f08fb9eb86f9c8dc34f55
1l1m0n <sv.antonov1@gmail.com>
2025-07-10 19:14:42 +0200

    Added 500 users + List<User> -> Page<User> + searching by name
    Added:
    -funkcja testowa do generacji użytkowników
    -dodana obsługa paginacji
    -wyszukiwanie findByNameIn - wyszukiwanie poprzez nazwę użytkownika, można szukać od razu kilku użytkowników na raz

--- hello/src/main/java/com/antonovproject/hello/HelloController.java
+++ hello/src/main/java/com/antonovproject/hello/HelloController.java
@@ -1,28 +1,28 @@
 package com.antonovproject.hello;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class HelloController {
+
+    @Autowired
+    private UserRepo userRepo;
     
     @RequestMapping("/helloWorld")
     public String helloWorld() {
         return "Hello Antonov!!!!!";
     }
 
-    @Autowired
-    private UserRepo userRepo;
-
-    @PostMapping("/addUser")
+    @GetMapping("/addUser")
     public String addUser(@RequestParam String name) {
 
         User user = new User();
@@ -30,9 +30,23 @@ public class HelloController {
         userRepo.save(user);
         return "User added and save: " + name;
     }
+
+    @GetMapping("/generateUsers")
+    public String generateUsers() {
+        for (int i = 0; i < 500; i++) {
+            User user = new User();
+            user.setName("user" + i);
+            userRepo.save(user);
+        }
+        return "500 users added to DB";
+    }
     
     @GetMapping("/getUsers")
-    public List<User> getUsers() {
-        return userRepo.findAll();
+    public Page<User> getUsers(@RequestParam(required = false) List<String> name, Pageable pageable) {
+        if (name != null && !name.isEmpty()) {
+            return userRepo.findByNameIn(name, pageable);
+        } else {
+            return userRepo.findAll(pageable);
+        }
     }
 }
--- hello/src/main/java/com/antonovproject/hello/UserRepo.java
+++ hello/src/main/java/com/antonovproject/hello/UserRepo.java
@@ -1,8 +1,13 @@
 package com.antonovproject.hello;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface UserRepo extends JpaRepository<User, Long>{
 
+    Page<User> findByNameIn(List<String> name, Pageable pageable);
 }
