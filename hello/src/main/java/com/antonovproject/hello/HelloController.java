4ac12a61603b3fa3805a524b42603c3a04082eb7
1l1m0n <sv.antonov1@gmail.com>
2025-07-20 10:51:33 +0200

    Edit + Delete
    Dodano:
    -pola firstName i lastName
    -metoda editUser() z możliwością edytowania przez ID
    -metoda deleteUsers() z możliwością usuwania kilku użytkowników na raz
    -metody do repozytorium (findByFirstNameIn(), findByLastNameIn(), findByIdIn())
    -metody do repozytorium do aktualizacji bazy danych (updateFirstNameById(), updateLastNameById()) wraz z zapytaniami SQL
    -testowa metoda deleteAllUsers()

--- hello/src/main/java/com/antonovproject/hello/HelloController.java
+++ hello/src/main/java/com/antonovproject/hello/HelloController.java
@@ -6,10 +6,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class HelloController {
@@ -22,31 +26,72 @@ public class HelloController {
         return "Hello Antonov!!!!!";
     }
 
-    @GetMapping("/addUser")
-    public String addUser(@RequestParam String name) {
+    @PostMapping("/addUser")
+    public String addUser(@RequestParam String firstName, @RequestParam(required=false) String lastName) {
 
         User user = new User();
-        user.setName(name);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
         userRepo.save(user);
-        return "User added and save: " + name;
+        return "User added and save: " + firstName + " " + lastName;
     }
 
-    @GetMapping("/generateUsers")
+    @PostMapping("/generateUsers")
     public String generateUsers() {
         for (int i = 0; i < 500; i++) {
             User user = new User();
-            user.setName("user" + i);
+            user.setFirstName("user" + i);
+            user.setLastName("lastName" + i);
             userRepo.save(user);
         }
         return "500 users added to DB";
     }
     
     @GetMapping("/getUsers")
-    public Page<User> getUsers(@RequestParam(required = false) List<String> name, Pageable pageable) {
-        if (name != null && !name.isEmpty()) {
-            return userRepo.findByNameIn(name, pageable);
+    public Page<User> getUsers(@RequestParam(required = false) List<String> firstName,
+            @RequestParam(required = false) List<String> lastName, @RequestParam(required = false) List<Long> id, Pageable pageable) {
+        if (firstName != null && !firstName.isEmpty()) {
+            return userRepo.findByFirstNameIn(firstName, pageable);
+        } else if (lastName != null && !lastName.isEmpty()) {
+            return userRepo.findByLastNameIn(lastName, pageable);
+        } else if (id != null && !id.isEmpty()) {
+            return userRepo.findByIdIn(id, pageable);
         } else {
             return userRepo.findAll(pageable);
         }
+    }
+
+    @PostMapping("/editUser")
+    public String editUser(@RequestParam Long id, @RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName) {
+
+        int updated = 0;
+
+        if (userRepo.findById(id).isEmpty()) {
+            return "User " + id + " not found";
+        }
+
+        if (firstName != null) {
+            updated += userRepo.updateFirstNameById(id, firstName);
+        }
+        if (lastName != null) {
+            updated += userRepo.updateLastNameById(id, lastName);
+        }
+        if (updated > 0) {
+            return "User updated: " + id;
+        } else {
+            return "Nothing to update";
+        }
+    }
+
+    @DeleteMapping("/deleteUsers")
+    public String deleteUsers(@RequestParam List<Long> id) {
+        userRepo.deleteAllByIdInBatch(id);
+        return "Deleted users: " + id;
+    }
+
+    @DeleteMapping("/deleteAllUsers")
+    public String deleteAllUsers() {
+        userRepo.deleteAll();
+        return "All users deleted";
     }
 }
